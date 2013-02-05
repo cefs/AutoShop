d3a39214f05ab52ac9ee3ffdb25123b5a73afe5c
Carlos Silva <cefs1984@gmail.com>
2013-02-05 19:45:50 -0200

    chapter 5.2

--- app/controllers/anuncios_controller.rb
+++ app/controllers/anuncios_controller.rb
@@ -1,2 +1,12 @@
 class AnunciosController < ApplicationController
+	def create
+		anuncio = Anuncio.new params[:anuncio]
+
+		if anuncio.save
+			flash[:notice] = "criado com sucesso!"
+		end
+
+		redirect_to root_path
+
+	end
 end
