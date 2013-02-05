4ebb54df1e432e93a43292ea1866a340d16e519c
Carlos Silva <cefs1984@gmail.com>
2013-02-04 22:25:01 -0200

    chapter 3.7

---
+++ app/models/anuncio.rb
@@ -0,0 +1,3 @@
+class Anuncio < ActiveRecord::Base
+  attr_accessible :ano, :descricao, :descricao, :modelo, :valor
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 AutoShop::Application.routes.draw do
+  
+  resources :anuncios
+
+
   get "loja/principal"
   root to: "loja#principal"
 end
