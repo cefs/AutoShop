e059db6ab71a895334e21570d6f271a8c0775a18
CARLOS SILVA <cefs1984@gmail.com>
2013-02-08 02:28:56 -0200

    correction in view  of anuncios and add Feeds of example

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#encoding:utf-8
+
+	#CRIANDO USUÁRIO ADMIN
+	user = User.new nome: "Administrador", email: "admin@example.org", password: "admin123"
+	user.role = "admin"
+
+	unless user.save
+	 	user.errors.full_messages.each do |msg|
+	 		puts msg
+	 	end
+	else
+		puts " :::::::::::::::::::::::::::::::::::::::: "
+		puts " :: 1 USUÁRIO ADMINSITRADOR DE EXEMPLO :: "
+		puts " :::::::::::::::::::::::::::::::::::::::: "
+
+		puts "email: #{user.email} e password: #{user.password}"	
+	end
+
+
+	#CRIANDO 3 MARCAS
+	puts " ::::::::::::::::::::::::::::::::::::::: "
+	puts " :: 3 MARCAS DE EXEMPLO FORAM CRIADAS :: "
+	puts " ::::::::::::::::::::::::::::::::::::::: "
+
+
+	["Fiat","Ford","Wolks"].each do |item|
+		Marca.create nome: "#{item}"
+	end
+
+
+	#CRIANDO 3 ANÚNCIOS
+	puts " ::::::::::::::::::::::::::::::::::::::::: "
+	puts " :: 3 ANÚNCIOS DE EXEMPLO FORAM CRIADOS :: "
+	puts " ::::::::::::::::::::::::::::::::::::::::: "
+
+	i = 0
+	["Uno","Focus","Gol"].each do |item|
+		i = i+1
+		anuncio = Anuncio.new modelo: "#{item}", ano: "2013", valor: 2000, descricao:"Conservado", marca_id: i
+		anuncio.aprovado = true
+		anuncio.save
+	end	
+
